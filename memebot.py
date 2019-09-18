e148be639f952f6eecdc1b5a8cdf4e4a5fc9c0c9
HunterKing <hking0036@gmail.com>
2019-09-18 10:14:07 -0400

    Change from relative path listing to absolute, add forceupdate in case of issues with array

--- memebot.py
+++ memebot.py
@@ -11,6 +11,7 @@ bot = commands.Bot(command_prefix='!', help_command = None)
 TOKEN = '' #Set token to load bot.
 ADMIN_ROLE = '' #Set Admin role for some commands. 
 GAME = discord.Game("Joel-san, Daisuke!! ~Joel Swanson Dating Simulator~") #Game title for bot.
+IMAGE_PATH = '/home/hunter/Documents/meme/Discord-Memebot/images/' #when running as a systemd service, relative paths have some trouble.
 num_files = 0
 image_names = []
 
@@ -19,7 +20,7 @@ def enumeratefiles():
     global image_names 
     num_files = 0 #since we will traverse the entire folder again, we must reset num_files.
     image_names = []
-    for root, dirs, files in os.walk("./images"):
+    for root, dirs, files in os.walk(IMAGE_PATH):
         for filename in files:
             image_names.append(filename)
             num_files += 1
@@ -71,13 +72,19 @@ async def freshtap(ctx):
 
 @bot.command()
 async def card(ctx):
-    imagetosend = "./images/" + fetchRandom()
+    imagetosend = IMAGE_PATH + fetchRandom()
     await ctx.send(file=discord.File(imagetosend))
   
 @bot.command()
+async def forceupdate(ctx):
+    global num_files
+    enumeratefiles()
+    await ctx.send('```Done.```')
+
+@bot.command()
 async def numcards(ctx):
     global num_files
-    str = ("```There are {} cards.```").format(num_files)
+    str = ('```There are {} cards.```').format(num_files)
     await ctx.send(str)
 
 @bot.command()
@@ -87,7 +94,7 @@ async def addcard(ctx, arg):
         await ctx.send("```You did not provide a valid URL.```")
     else:
         try:
-            wget.download(url[0], './images/')
+            wget.download(url[0], IMAGE_PATH)
             enumeratefiles()
             await ctx.send("```New card downloaded.```")
         except urllib.error.HTTPError as e:
