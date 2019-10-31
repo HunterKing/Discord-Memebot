394b897c1edd3c840fb82d4f14834ef1a60ef541
HunterKing <hking0036@gmail.com>
2019-10-31 01:06:43 -0400

    remove incorrect usage text on addcard.js

--- commands/addcard.js
+++ commands/addcard.js
@@ -3,7 +3,7 @@ const isImageUrl = require('is-image-url');
 module.exports = {
 	name: "addcard",
 	description: "Add a card to the bot.",
-	usage: '[command name] <url>',
+	usage: '<url>',
 	execute(message) {
 		message.channel.send("In progress...");
 	},
