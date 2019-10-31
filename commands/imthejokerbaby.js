20cd79602d31d570bd222406c8b48da4d5a4c171
HunterKing <hking0036@gmail.com>
2019-10-31 16:49:18 -0400

    Fix joe story's issues

--- commands/imthejokerbaby.js
+++ commands/imthejokerbaby.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "imthejokerbaby",
     description: "Links to Heath Ledgers performance of the Joker",
     execute(message) {
-        message.channel.send("https://https://www.youtube.com/watch?v=DX_wprkggoE.youtube.com/watch?v=jjOXQMFn5mM");
+        message.channel.send("https://www.youtube.com/watch?v=DX_wprkggoE");
     },
 };
