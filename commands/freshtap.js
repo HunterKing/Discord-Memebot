070dec7c187c18ac56090dff872ecd0607253e89
Hunter King <hking0036@gmail.com>
2019-10-31 16:57:42 -0400

    update freshtap

--- commands/freshtap.js
+++ commands/freshtap.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: "freshtap",
 	description: "Links instructional recipe for tap water.",
 	execute(message) {
-		message.channel.send("https://www.youtube.com/watch?v=BdzShsSspC8");
+		message.channel.send("https://www.youtube.com/watch?v=PvIky3B661s");
 	},
 };
