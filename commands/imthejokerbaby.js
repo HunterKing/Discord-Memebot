7f40c62be19da03676ccbc4d7ba492a60a962560
storyjss <lefoustory@gmail.com>
2019-10-31 16:38:48 -0400

    Adding a command for the character of the century

---
+++ commands/imthejokerbaby.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "imthejokerbaby",
+    description: "Links to Heath Ledgers performance of the Joker",
+    execute(message) {
+        message.channel.send("https://https://www.youtube.com/watch?v=DX_wprkggoE.youtube.com/watch?v=jjOXQMFn5mM");
+    },
+};
