c9e50cd1293592939e63205a73204e5e97943167
HunterKing <hking0036@gmail.com>
2020-02-03 12:38:06 -0500

    add simp

---
+++ commands/simp.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: "simp",
+	description: "Informs you of unfortunate circumstances.",
+	execute(message) {
+		message.channel.send("https://www.youtube.com/watch?v=xq3bPcN_AQ4");
+	},
+};
