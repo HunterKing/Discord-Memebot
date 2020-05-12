f34217507061aeb0d84a0787bcb689e9db3d41b5
Hunter King <hking0036@gmail.com>
2020-05-12 18:37:00 -0400

    add mvp, remove addcard as it didn't work and resulted in a vulnerable dependency

---
+++ commands/mvp.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: "mvp",
+	description: "Shouts out to the MVP from Faze",
+	execute(message){
+		message.channel.send("https://www.youtube.com/watch?v=r3T8WbthnZ4");
+	},
+};
